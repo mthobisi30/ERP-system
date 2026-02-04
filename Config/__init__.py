274536ed5d844a443fe3a10b5456b5b24552d882
Mthobsi Nxumalo <mthobosinx@icloud.com>
2026-02-04 08:47:18 +0200

    feat: Initialize Config directory as a Python package.

--- Config/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Configuration Package
-"""
-from .config import Config, DevelopmentConfig, ProductionConfig
-from .database import db
-
-__all__ = ['Config', 'DevelopmentConfig', 'ProductionConfig', 'db']
